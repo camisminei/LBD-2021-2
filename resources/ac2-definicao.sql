283911fd5380c4dafbfcf63794ae59d9e2ceea84
Camila Minei <camisminei.ufms@gmail.com>
2021-08-23 09:18:03 -0400

    ac2 ldb

---
+++ resources/ac2-definicao.sql
@@ -0,0 +1,30 @@
+--1. Definição do esquema
+
+
+--2. Definição de tabelas como chaves primárias. 
+
+-- TABELA DEPARTAMENTO
+
+-- TABELA DIVISAO
+
+-- TABELA EMPREGADO
+
+-- TABELA VENCIMENTO
+	-- vencimento.valor  - possui um valor default de Zero
+
+
+-- TABELA DEPENDENTE
+	-- No atributo sexo coloque uma restrição de check para validar que o atributo possui os valores 'M' ou 'F'
+
+
+-- TABELA DESCONTO
+
+-- TABELA EMP_DESC
+
+-- TABELA EMP_VENC
+
+--3. definição de chaves estrangeiras. Considere que todas as ações de remoção resultam em propagação.
+
+
+--4. Adicione (utilizando alter table) a coluna sexo na tabela dependente, char(1), para possuir os valores 'M' ou 'F'
+
