cd456800e8a8ec14cbcc7da4fcdf5d3a6944313d
Camila Minei <camisminei.ufms@gmail.com>
2021-11-21 15:28:51 -0400

    banco aav 14 trigger

---
+++ resources/PT1-produtos_categoria-trigger-dataset.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS produto cascade;
+DROP TABLE IF EXISTS categoria cascade;
+DROP TABLE IF EXISTS produto_log cascade;
+
+CREATE TABLE categoria (
+  id numeric PRIMARY KEY,
+  nome varchar(255)
+);
+
+CREATE TABLE produto (
+  id numeric PRIMARY KEY,
+  nome varchar (255),
+  quantidade numeric,
+  preco numeric,
+  id_categoria numeric NULL,
+  FOREIGN KEY (id_categoria) REFERENCES categoria (id)
+);
+
+CREATE TABLE produto_log(
+  id_produto integer, 
+  preco_antigo numeric, 
+  preco_novo numeric, 
+  data_alteracao timestamp,
+  tipo char(1) NOT NULL,
+  check (tipo in ('U','D'))
+);
+
+INSERT INTO categoria (id, nome) VALUES (1, 'Superior');
+INSERT INTO categoria (id, nome) VALUES (2, 'Super Luxury');
+INSERT INTO categoria (id, nome) VALUES (3, 'Modern');
+INSERT INTO categoria (id, nome) VALUES (4, 'Nerd');
+INSERT INTO categoria (id, nome) VALUES (5, 'Infantile');
+INSERT INTO categoria (id, nome) VALUES (6, 'Robust');
+INSERT INTO categoria (id, nome) VALUES (9, 'Wood');
+
+INSERT INTO produto (id, nome, quantidade, preco, id_categoria) VALUES (1, 'Blue Chair', 30, 300.00, 9);
+INSERT INTO produto (id, nome, quantidade, preco, id_categoria) VALUES (2, 'Red Chair',  200,  2150.00, 2);
+INSERT INTO produto (id, nome, quantidade, preco, id_categoria) VALUES (3, 'Disney Wardrobe',  400,  829.50, 4);
+INSERT INTO produto (id, nome, quantidade, preco, id_categoria) VALUES (4, 'Blue Toaster', 20, 9.90, 3);
+INSERT INTO produto (id, nome, quantidade, preco, id_categoria) VALUES (5, 'Solar Panel',  30, 3000.25,  4);
+INSERT INTO produto (id, nome, quantidade, preco, id_categoria) VALUES (6, 'Red Toaster',  30, 9.25,  NULL);
---
+++ resources/PT1-produtos_categoria-trigger-resposta.sql
@@ -0,0 +1,17 @@
+-- 1. Crie um trigger chamado produtolog que chame uma function com o mesmo nome.
+-- Esse trigger deve ser disparado sempre que for alterado o preço do produto. Nesse caso, uma tupla é inserida na tabela produto_log
+-- Dica: Utilize CURRENT_DATE para inserir a data atual; Considere na tabela produto_log os tipos U (Update) e D (delete)
+
+  -- Definição da function
+
+
+  --Definição do trigger
+
+
+-- 2. Crie os comandos SQL necessários para testar todas as opções de ação do seu trigger
+  
+
+-- 3. Crie comandos SQL para apagar o trigger e a function
+
+
+
